25d3bb82ddc8ee19dcc33c2de816051df9071e10
MSI <nhannt1311@gmail.com>
2021-09-02 10:40:49 +0700

    first commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+    $server = "localhost";
+    $user= "root";
+    $password = "root";
+    $dbname = "phpblog";
+
+    $connect = mysqli_connect($server, $user, $password, $dbname);
+
+    if(!$connect){
+        die("Connect Failed!".mysqli_connect_error());
+    } else {
+        echo 'Connect Success!';
+    }
+?>
